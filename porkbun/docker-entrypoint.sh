590def5f3150525b557426b09354b3fa474ae717
Manuel Sabogal <mfer32@gmail.com>
2024-03-24 23:55:10 +0100

    [porkbun] Added time_update configuration

--- porkbun/docker-entrypoint.sh
+++ porkbun/docker-entrypoint.sh
@@ -4,9 +4,10 @@ api_key="$(bashio::config 'api_key')"
 secret_key="$(bashio::config 'secret_key')"
 domain="$(bashio::config 'domain')"
 subdomains="$(bashio::config 'subdomains')"
+time_update="$(bashio::config 'time_update')"
 
 secrets_json_fmt='{"api_key": "%s", "secret_key": "%s"}'
 printf -v secrets_json "$secrets_json_fmt" $api_key $secret_key
 echo "$secrets_json" > secrets.json
 
-./porkbun_ddns_rs $domain --subdomains "$subdomains" --secrets secrets.json
+./porkbun_ddns_rs "$domain" --subdomains "$subdomains" --secrets secrets.json -t "$time_update"
