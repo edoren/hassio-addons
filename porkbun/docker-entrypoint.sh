4bdf0f26794cb3a57ec89ca24ecee7a69ae756c1
Manuel Sabogal <mfer32@gmail.com>
2024-07-22 16:01:35 +0200

    [porkbun] Improve highly verbose output

--- porkbun/docker-entrypoint.sh
+++ porkbun/docker-entrypoint.sh
@@ -10,4 +10,11 @@ secrets_json_fmt='{"api_key": "%s", "secret_key": "%s"}'
 printf -v secrets_json "$secrets_json_fmt" $api_key $secret_key
 echo "$secrets_json" > secrets.json
 
+echo "============================================================"
+echo "$(./porkbun_ddns_rs --version)"
+echo ""
+echo "  Domain: $domain"
+echo "  Subdomains: $subdomains"
+echo "============================================================"
+
 ./porkbun_ddns_rs "$domain" --subdomains "$subdomains" --secrets secrets.json -t "$time_update"
