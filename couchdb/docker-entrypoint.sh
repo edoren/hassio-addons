7446f9d32bd228cf7f050d1dfd9128886beb774d
Manuel Sabogal <mfer32@gmail.com>
2024-11-18 20:08:32 +0100

    [couchdb] Make sure to set the right DB folder

--- couchdb/docker-entrypoint.sh
+++ couchdb/docker-entrypoint.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
-set -e
-
-if [ ! -L '/opt/couchdb/etc/local.ini' ]; then
-	if [ ! -f '/config/local.ini' ]; then
-		mv /opt/couchdb/etc/local.ini /config
-	else
-		rm /opt/couchdb/etc/local.ini
-	fi
-	ln -s /config/local.ini /opt/couchdb/etc/local.ini
-fi
-
-if [ "$1" = '/opt/couchdb/bin/couchdb' ]; then
-	touch /opt/couchdb/etc/local.d/docker.ini
-
-	if ! grep -Pzoqr "\[couchdb\]\ndatabase_dir =" /opt/couchdb/etc/local.d/*.ini /opt/couchdb/etc/local.ini; then
-		mkdir -p /data/data
-		chown couchdb:couchdb /data/data
-		chmod 777 /data/data
-		printf "\n[couchdb]\ndatabase_dir = /data/data\n" >> /opt/couchdb/etc/local.d/docker.ini
-	fi
-fi
+echo "" > /opt/couchdb/etc/local.d/docker.ini
+mkdir -p /data/data
+chown couchdb:couchdb /data/data
+chmod 777 /data/data
+printf "\n[couchdb]\ndatabase_dir = /data/data\n" >> /opt/couchdb/etc/local.d/docker.ini
 
 source /usr/local/bin/docker-entrypoint.sh "$1"
