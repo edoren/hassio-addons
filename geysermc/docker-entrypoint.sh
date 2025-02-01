e06bb3e72e663abb5385e26bb0397723b8c156da
Manuel Sabogal <mfer32@gmail.com>
2025-02-01 19:05:36 +0100

    [geyser] Added more verbose output

--- geysermc/docker-entrypoint.sh
+++ geysermc/docker-entrypoint.sh
@@ -6,6 +6,8 @@ config_version="$(bashio::config 'version')"
 config_min_memory="$(bashio::config 'min_memory')"
 config_max_memory="$(bashio::config 'max_memory')"
 
+echo "Checking last version";
+
 latest_build_info=$(wget -O- --no-check-certificate --quiet --method GET --timeout=0 --header "Accept: application/json" "https://download.geysermc.org/v2/projects/geyser/versions/${config_version}/builds/latest")
 if [ -z "$latest_build_info" ]; then
     echo "Invalid Geyser version, select a proper version";
@@ -20,8 +22,9 @@ if [ -f "$LAST_VERSION_FILE" ]; then
     file_contents=$(cat "$LAST_VERSION_FILE");
     file_version=$(echo "$file_contents" | jq ".version");
     file_build=$(echo "$file_contents" | jq ".build");
-
+    echo "Last executed version $file_version build $file_build";
     if [[ $file_version != $response_version ]] || [[ $file_build != $response_build ]]; then
+        echo "Newer version found $response_version build $response_build";
         download_latest_version=1;
     fi
 else
@@ -31,7 +34,7 @@ fi
 if [[ $download_latest_version -eq 1 ]]; then
     echo "Downloading latest version $response_version build $response_build";
     mkdir -p /opt/geyser;
-    wget -O /opt/geyser/Geyser.jar --content-disposition "https://download.geysermc.org/v2/projects/geyser/versions/${config_version}/builds/latest/downloads/standalone";
+    wget -O /opt/geyser/Geyser.jar --content-disposition --quiet "https://download.geysermc.org/v2/projects/geyser/versions/${config_version}/builds/latest/downloads/standalone";
     jq -n --arg version "$response_version" --arg build "$response_build" '{version: $version, build: $build}' > $LAST_VERSION_FILE;
 fi
 
