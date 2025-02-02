9eecf6741a8321c82a0c62a018ac2e685aa9aee7
Manuel Sabogal <personal@edoren.me>
2025-02-02 04:20:08 +0100

    [rallly] Declare and assign separately to avoid masking return values SC2155
    https://www.shellcheck.net/wiki/SC2155

--- rallly/docker-entrypoint.sh
+++ rallly/docker-entrypoint.sh
@@ -5,19 +5,38 @@ database_host="$(bashio::config 'database_host')"
 database_port="$(bashio::config 'database_port')"
 database_user="$(bashio::config 'database_user')"
 database_password="$(bashio::config 'database_password')"
-
 export DATABASE_URL="postgres://$database_user:$database_password@$database_host:$database_port/$database_name"
-export SECRET_PASSWORD="$(bashio::config 'secret_password')"
-export NEXT_PUBLIC_BASE_URL="$(bashio::config 'next_public_base_url')"
-export ALLOWED_EMAILS="$(readarray -t ARRAY <<< "$(bashio::config 'allowed_emails')"; IFS=','; echo "${ARRAY[*]}")"
-if [ ! -z "$(bashio::config 'noreply_email')" ]; then
-    export NOREPLY_EMAIL="$(bashio::config 'noreply_email')"
+
+secret_password="$(bashio::config 'secret_password')"
+export SECRET_PASSWORD="$secret_password"
+
+next_public_base_url="$(bashio::config 'next_public_base_url')"
+export NEXT_PUBLIC_BASE_URL="$next_public_base_url"
+
+allowed_emails="$(readarray -t ARRAY <<< "$(bashio::config 'allowed_emails')"; IFS=','; echo "${ARRAY[*]}")"
+export ALLOWED_EMAILS="$allowed_emails"
+
+if [ -n "$(bashio::config 'noreply_email')" ]; then
+    noreply_email="$(bashio::config 'noreply_email')"
+    export NOREPLY_EMAIL="$noreply_email"
 fi
-export SUPPORT_EMAIL="$(bashio::config 'support_email')"
-export SMTP_HOST="$(bashio::config 'smtp_host')"
-export SMTP_PORT="$(bashio::config 'smtp_port')"
-export SMTP_SECURE="$(bashio::config 'smtp_secure')"
-export SMTP_USER="$(bashio::config 'smtp_user')"
-export SMTP_PWD="$(bashio::config 'smtp_pwd')"
+
+support_email="$(bashio::config 'support_email')"
+export SUPPORT_EMAIL="$support_email"
+
+smtp_host="$(bashio::config 'smtp_host')"
+export SMTP_HOST="$smtp_host"
+
+smtp_port="$(bashio::config 'smtp_port')"
+export SMTP_PORT="$smtp_port"
+
+smtp_secure="$(bashio::config 'smtp_secure')"
+export SMTP_SECURE="$smtp_secure"
+
+smtp_user="$(bashio::config 'smtp_user')"
+export SMTP_USER="$smtp_user"
+
+smtp_pwd="$(bashio::config 'smtp_pwd')"
+export SMTP_PWD="$smtp_pwd"
 
 /app/docker-start.sh
